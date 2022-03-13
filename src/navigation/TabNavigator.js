73773f8b91ae9b2261b4c8ae3bf9ecd391ae2b30
Bekir Yörük <bekiryoruk1@gmail.com>
2022-03-13 19:22:34 +0300

    fixed merge fault

--- src/navigation/TabNavigator.js
+++ src/navigation/TabNavigator.js
@@ -1,11 +1,10 @@
 import * as React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
+import Main from '../pages/Main';
 import CameraScreen from '../pages/CameraScreen';
-import Information from '../pages/Information';
 import Calibration from '../pages/Calibration';
 import IconButton from './IconButton';
-import StackNavigator from './StackNavigator';
 
 const Tab = createBottomTabNavigator();
 
@@ -14,35 +13,52 @@ function TabNavigator() {
     <Tab.Navigator
       tabBarOptions={{
         showLabel: false,
-        keyboardHidesTabBar: false,
         style: {
           position: 'absolute',
           bottom: 0,
           backgroundColor: '#ffffff',
-          height: 40,
-        }
-      }}
-    >
-      <Tab.Screen name="Home" component={StackNavigator} options= {{
-        tabBarIcon: ({focused}) => (
-          <IconButton text={'Home'} src={require('../assets/navigation/home.png')} focused={focused}/>
-        )
-      }} />
-      <Tab.Screen name="CameraScreen" component={CameraScreen} options= {{
-        tabBarIcon: ({focused}) => (
-          <IconButton text={'Camera'} src={require('../assets/navigation/camera.png')} focused={focused}/>
-        )
-      }}/>
-      <Tab.Screen name="Information" component={Information} options= {{
-        tabBarIcon: ({focused}) => (
-          <IconButton text={'Information'} src={require('../assets/navigation/t.png')} focused={focused}/>
-        )
-      }}/>
-      <Tab.Screen name="Calibration" component={Calibration} options= {{
-        tabBarIcon: ({focused}) => (
-          <IconButton text={'Calibration'} src={require('../assets/navigation/settings.png')} focused={focused}/>
-        )
-      }}/>
+          height: 60,
+        },
+      }}>
+      <Tab.Screen
+        name="Home"
+        component={Main}
+        options={{
+          tabBarIcon: ({focused}) => (
+            <IconButton
+              text={'Home'}
+              src={require('../assets/navigation/home.png')}
+              focused={focused}
+            />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="CameraScreen"
+        component={CameraScreen}
+        options={{
+          tabBarIcon: ({focused}) => (
+            <IconButton
+              text={'Camera'}
+              src={require('../assets/navigation/camera.png')}
+              focused={focused}
+            />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Calibration"
+        component={Calibration}
+        options={{
+          tabBarIcon: ({focused}) => (
+            <IconButton
+              text={'Calibration'}
+              src={require('../assets/navigation/calibrationn.png')}
+              focused={focused}
+            />
+          ),
+        }}
+      />
     </Tab.Navigator>
   );
 }
