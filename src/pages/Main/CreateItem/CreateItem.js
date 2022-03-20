5859f9d1e7a74f05622e0fc46a2c8fb55322b006
Bekir Yörük <bekiryoruk1@gmail.com>
2022-03-20 14:34:34 +0300

    added select field

--- src/pages/Main/CreateItem/CreateItem.js
+++ src/pages/Main/CreateItem/CreateItem.js
@@ -2,14 +2,15 @@ import React, {useState} from 'react';
 import {SafeAreaView, Text, Button, Alert} from 'react-native';
 import CustomTextInput from '../../../components/CustomTextInput/CustomTextInput';
 import {storeItem, getItem} from '../../../helpers';
+import SelectDropdown from 'react-native-select-dropdown';
 
 import styles from './CreateItem.styles';
-
 
 const CreateItem = () => {
   const [type, setType] = useState('');
   const [firstItem, setFirstItem] = useState('');
   const [secondItem, setSecondItem] = useState('');
+  const types = ['contacts', 'youtube', 'spotify'];
 
   const storeNewItem = () => {
     if (firstItem.length === 0 || secondItem.length === 0) {
@@ -32,6 +33,22 @@ const CreateItem = () => {
   return (
     <SafeAreaView>
       <Text style={styles.sectionTitle}>Create Item</Text>
+      <SelectDropdown
+        data={types}
+        onSelect={selectedItem => {
+          setType(selectedItem);
+        }}
+        buttonTextAfterSelection={(selectedItem, index) => {
+          // text represented after item is selected
+          // if data array is an array of objects then return selectedItem.property to render after item is selected
+          return selectedItem;
+        }}
+        rowTextForSelection={(item, index) => {
+          // text represented for each item in dropdown
+          // if data array is an array of objects then return item.property to represent item in dropdown
+          return item;
+        }}
+      />
       <CustomTextInput
         onChange={e => setFirstItem(e)}
         value={firstItem}
