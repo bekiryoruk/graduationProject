606dbb8a4a3ca26ed33535ccffb5cc7b583803f4
Bekir Yörük <bekiryoruk1@gmail.com>
2022-03-26 15:25:41 +0300

    added local storage to save user type

--- src/pages/Modals/SecondModal/SecondModal.js
+++ src/pages/Modals/SecondModal/SecondModal.js
@@ -1,9 +1,16 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import {View, Text, Button} from 'react-native';
 import {CustomTextInput} from '../../../components';
+import {getItem} from '../../../helpers';
 
 const SecondModal = ({navigation}) => {
   const [firstItem, setFirstItem] = useState('');
+
+  useEffect(() => {
+    const userType = getItem('userType').then(data => {
+      console.log(data);
+    });
+  }, []);
 
   return (
     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
