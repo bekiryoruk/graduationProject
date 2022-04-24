e46d4ad367e1a6495b85f8bdddca75e462aee9f4
Bekir Yörük <bekiryoruk1@gmail.com>
2022-04-24 13:34:25 +0300

    removed unnecessarry codes

--- src/pages/Home/VoiceDisable/VoiceDisable.js
+++ src/pages/Home/VoiceDisable/VoiceDisable.js
@@ -1,17 +1,6 @@
 import React from 'react';
-import {
-  StyleSheet,
-  Text,
-  View,
-  TouchableOpacity,
-  Dimensions,
-  Platform,
-  Linking,
-} from 'react-native';
+import {Text, View, TouchableOpacity, Platform, Linking} from 'react-native';
 import {RNCamera} from 'react-native-camera';
-import RNCalendarEvents from 'react-native-calendar-events';
-import Voice from '@react-native-community/voice';
-import BackgroundService from 'react-native-background-actions';
 
 import styles from './VoiceDisable.styles';
 import {callPhone, sendSMS, getItem} from '../../../helpers';
@@ -19,81 +8,20 @@ import {BackPressHandler} from '../../../components';
 const landmarkSize = 10; // NOTE: bunu değiştirirsen style dosyasından da değişiklik yap, * landmark *
 export default class VoiceDisable extends React.Component {
   state = {
-    flash: 'off',
-    zoom: 0,
-    autoFocus: 'on',
-    autoFocusPoint: {
-      normalized: {x: 0.5, y: 0.5}, // normalized values required for autoFocusPointOfInterest
-      drawRectPosition: {
-        x: Dimensions.get('window').width * 0.5 - 32,
-        y: Dimensions.get('window').height * 0.5 - 32,
-      },
-    },
-    depth: 0,
-    type: 'front', // Camera Front or Back
-    whiteBalance: 'auto',
-    ratio: '16:9',
     canDetectFaces: false,
-    canDetectText: false,
     buttonsHover: {
       callButton: true,
       smsButton: false,
       musicButton: false,
       videoButton: false,
-      calenderButton: false,
     },
-    canDetectBarcode: false,
     faces: [],
     blinkDetected: false,
-    blinkedimage: null,
-    voiceResult: '',
     phoneNumber: '',
     musicLink: '',
     videoLink: '',
   };
 
-  sleep = time => new Promise(resolve => setTimeout(() => resolve(), time));
-
-  veryIntensiveTask = async taskDataArguments => {
-    // Example of an infinite loop task
-    const {delay} = taskDataArguments;
-    await new Promise(async resolve => {
-      for (let i = 0; BackgroundService.isRunning(); i++) {
-        console.log(this.state.voiceResult);
-        await this.sleep(delay);
-      }
-    });
-  };
-
-  options = {
-    taskName: 'Example',
-    taskTitle: 'Touchless',
-    taskDesc: 'Voice Recording',
-    taskIcon: {
-      name: 'ic_launcher',
-      type: 'mipmap',
-    },
-    color: '#ff00ff',
-    linkingURI: 'yourSchemeHere://chat/jane', // See Deep Linking for more info
-    parameters: {
-      delay: 2000,
-    },
-  };
-
-  backgroundServiceStart = async () => {
-    await BackgroundService.start(this.veryIntensiveTask, this.options);
-  };
-
-  /*b = async () => {
-    await BackgroundService.updateNotification({
-      taskDesc: 'Voice Recording',
-    });
-  };
-
-  c = async () => {
-    await BackgroundService.stop();
-  };*/
-  // TODO: bu kısım uyarı veriyor bu kısıma dönücem
   UNSAFE_componentWillMount = async () => {
     if (Platform.OS == 'android') {
       BackPressHandler(this.BackStuff);
@@ -118,129 +46,11 @@ export default class VoiceDisable extends React.Component {
         musicLink: returnMusicData && returnMusicData[0]?.param,
       });
     }
-    Voice.onSpeechStart = this.onSpeechStartHandler;
-    Voice.onSpeechEnd = this.onSpeechEndHandler;
-    Voice.onSpeechResults = this.onSpeechResultsHandler;
-    this.startRecording();
-    return () => {
-      Voice.destroy().then(Voice.removeAllListeners);
-    };
   };
 
   BackStuff = () => {
     // console.log('back button pressed');
   };
-
-  onSpeechStartHandler = e => {
-    console.log('start handler==>>>', e);
-    this.backgroundServiceStart();
-  };
-
-  onSpeechEndHandler = e => {
-    //  TODO: stop ihtiyacım olursa diye tutuyorum simdilik
-  };
-
-  onSpeechResultsHandler = e => {
-    let text = e.value[0];
-    this.setState({
-      voiceResult: text,
-    });
-    console.log('speech result handler', e);
-    console.log(text);
-    if (text.includes('call')) {
-      this.callAnyone();
-    } else if (text.includes('music') || text.includes('spotify')) {
-      this.openSpotify();
-    } else if (text.includes('youtube') || text.includes('video')) {
-      this.openYoutube();
-    } else if (text.includes('sms') || text.includes('message')) {
-      this.sendSms();
-    } else if (text.includes('calendar') || text.includes('save')) {
-      this.setEventToCalender();
-    } else if (
-      text.includes('turn') ||
-      text.includes('back') ||
-      text.includes('touchless')
-    ) {
-      // bu kısım geliştirilecek
-      this.turnBackToApp();
-    }
-    this.startRecording();
-  };
-
-  startRecording = async () => {
-    try {
-      await Voice.start('en-Us');
-    } catch (error) {
-      console.log('error raised', error);
-    }
-  };
-
-  stopRecording = async () => {
-    try {
-      // await Voice.stop();
-    } catch (error) {
-      console.log('error raised', error);
-    }
-  };
-
-  toggleFacing() {
-    this.setState({
-      type: this.state.type === 'back' ? 'front' : 'back',
-    });
-  }
-
-  touchToFocus(event) {
-    const {pageX, pageY} = event.nativeEvent;
-    const screenWidth = Dimensions.get('window').width;
-    const screenHeight = Dimensions.get('window').height;
-    const isPortrait = screenHeight > screenWidth;
-
-    let x = pageX / screenWidth;
-    let y = pageY / screenHeight;
-    // Coordinate transform for portrait. See autoFocusPointOfInterest in docs for more info
-    if (isPortrait) {
-      x = pageY / screenHeight;
-      y = -(pageX / screenWidth) + 1;
-    }
-    this.setState({
-      autoFocusPoint: {
-        normalized: {x, y},
-        drawRectPosition: {x: pageX, y: pageY},
-      },
-    });
-  }
-
-  setFocusDepth(depth) {
-    this.setState({
-      depth,
-    });
-  }
-
-  takePicture = async function () {
-    /* if (this.camera) {
-      const data = await this.camera.takePictureAsync();
-      console.warn('takePicture ', data);
-      this.setState({blinkedimage: data.path});
-    }*/
-  };
-
-  /*manageButtonStates(type) {
-    this.setState({
-      buttonsHover: {
-        callButton: false,
-        smsButton: false,
-        musicButton: false,
-      },
-    });
-    if (type === 'call') {
-      this.setState({ buttonsHover: { callButton: true } });
-    } else if (type === 'sms') {
-      this.setState({ buttonsHover: { smsButton: true }});
-    } else {
-      this.setState({ buttonsHover: {  musicButton: true }});
-    }
-  }*/
 
   setTrueSmsButton() {
     this.setState({
@@ -249,7 +59,6 @@ export default class VoiceDisable extends React.Component {
         smsButton: true,
         musicButton: false,
         videoButton: false,
-        calenderButton: false,
       },
     });
   }
@@ -261,7 +70,6 @@ export default class VoiceDisable extends React.Component {
         smsButton: false,
         musicButton: false,
         videoButton: false,
-        calenderButton: false,
       },
     });
   }
@@ -273,7 +81,6 @@ export default class VoiceDisable extends React.Component {
         smsButton: false,
         musicButton: true,
         videoButton: false,
-        calenderButton: false,
       },
     });
   }
@@ -285,19 +92,6 @@ export default class VoiceDisable extends React.Component {
         smsButton: false,
         musicButton: false,
         videoButton: true,
-        calenderButton: false,
-      },
-    });
-  }
-
-  setTrueCalenderButton() {
-    this.setState({
-      buttonsHover: {
-        callButton: false,
-        smsButton: false,
-        musicButton: false,
-        videoButton: false,
-        calenderButton: true,
       },
     });
   }
@@ -308,11 +102,6 @@ export default class VoiceDisable extends React.Component {
 
   sendSms = async function () {
     sendSMS([this.state.phoneNumber], 'selam');
-    /* Whatsapp kısmını yoruma aldım ne yaparız burayı bilmıyom
-    const mobile = '+905345242175';
-    let url =
-      'whatsapp://send?text=' + 'denem-whatsapp-message' + '&phone=' + mobile;
-    Linking.openURL(url);*/
   };
 
   openSpotify = async function () {
@@ -321,26 +110,6 @@ export default class VoiceDisable extends React.Component {
 
   openYoutube = async function () {
     Linking.openURL(this.state.videoLink);
-  };
-
-  turnBackToApp = async function () {
-    // TODO: bunu araştırıp app'e dönmesini sağlıycam
-    Linking.openURL('Touchless');
-  };
-
-  setEventToCalender = async function () {
-    RNCalendarEvents.findCalendars();
-    RNCalendarEvents.saveEvent('Event Name', {
-      calendarId: '003',
-      startDate: '2022-03-15T13:42:00.000Z', // burada yazılan saatin 3 saat sonrasına etkinlik oluşturuyor.
-      endDate: '2022-03-15T14:39:00.000Z',
-      location: 'Izmir, Turkey',
-    });
-  };
-
-  toggle = value => () => {
-    this.setState(prevState => ({[value]: !prevState[value]}));
-    console.log(value, this.state[`${value}`]);
   };
 
   facesDetected = ({faces}) => {
@@ -360,23 +129,10 @@ export default class VoiceDisable extends React.Component {
       faces[0].leftEyePosition.x < 390
     ) {
       this.setTrueMusicButton();
-    } else if (
-      faces[0].leftEyePosition.x > 390 &&
-      faces[0].leftEyePosition.x < 430
-    ) {
-      this.setTrueCalenderButton();
     } else {
       this.setTrueVideoButton();
     }
 
-    // console.log(
-    //   JSON.stringify({
-    //     rightEyeOpenProbability: rightEye,
-    //     leftEyeOpenProbability: leftEye,
-    //     smilingProbability: smileprob,
-    //     blinkProb: bothEyes,
-    //   }),
-    // );
     if (bothEyes <= 0.3) {
       console.log(
         JSON.stringify({
@@ -392,53 +148,15 @@ export default class VoiceDisable extends React.Component {
         this.sendSms();
       } else if (this.state.buttonsHover.musicButton) {
         this.openSpotify();
-      } else if (this.state.buttonsHover.calenderButton) {
-        this.setEventToCalender();
       } else {
         this.openYoutube();
       }
     }
     if (this.state.blinkDetected && bothEyes >= 0.9) {
-      this.takePicture(faces);
       this.setState({blinkDetected: false});
     }
     this.setState({faces});
   };
-
-  renderFace = ({
-    bounds,
-    faceID,
-    rollAngle,
-    yawAngle,
-    leftEyeOpenProbability,
-    rightEyeOpenProbability,
-    smilingProbability,
-  }) => (
-    <View
-      key={faceID}
-      transform={[
-        {perspective: 600},
-        {rotateZ: `${rollAngle.toFixed(0)}deg`},
-        {rotateY: `${yawAngle.toFixed(0)}deg`},
-      ]}
-      style={[
-        styles.face,
-        {
-          ...bounds.size,
-          left: bounds.origin.x,
-          top: bounds.origin.y,
-        },
-      ]}>
-      <Text style={styles.faceText}>ID: {faceID}</Text>
-      <Text style={styles.faceText}>
-        eyeOpenProbability:
-        {leftEyeOpenProbability + rightEyeOpenProbability / 2}
-      </Text>
-      <Text style={styles.faceText}>
-        smilingProbability: {smilingProbability}
-      </Text>
-    </View>
-  );
 
   renderLandmarksOfFace(face) {
     const renderLandmark = position =>
@@ -473,13 +191,7 @@ export default class VoiceDisable extends React.Component {
 
   renderCamera() {
     const {canDetectFaces} = this.state;
-    const drawFocusRingPosition = {
-      top: this.state.autoFocusPoint.drawRectPosition.y - 32,
-      left: this.state.autoFocusPoint.drawRectPosition.x - 32,
-    };
-    // handleFaceDetected = faceArray => {
-    //   console.log('handleFaceDetected', faceArray);
-    // };
+
     return (
       <RNCamera
         ref={ref => {
@@ -489,9 +201,9 @@ export default class VoiceDisable extends React.Component {
           flex: 1,
           justifyContent: 'space-between',
         }}
-        type={this.state.type}
-        zoom={this.state.zoom}
-        ratio={this.state.ratio}
+        type="front"
+        zoom={0}
+        ratio="16:9"
         androidCameraPermissionOptions={{
           title: 'Permission to use camera',
           message: 'We need your permission to use your camera',
@@ -509,7 +221,6 @@ export default class VoiceDisable extends React.Component {
             : undefined
         }
         onCameraReady={() => {
-          console.log('onCameraReady');
           this.setState({canDetectFaces: true});
         }}
         onFacesDetected={this.state.canDetectFaces ? this.facesDetected : null}
@@ -520,22 +231,7 @@ export default class VoiceDisable extends React.Component {
             flex: 0.5,
             height: 72,
             backgroundColor: 'transparent',
-            flexDirection: 'row',
-            justifyContent: 'space-around',
-          }}>
-          <View
-            style={{
-              backgroundColor: 'transparent',
-              flexDirection: 'row',
-              justifyContent: 'space-around',
-            }}>
-            <TouchableOpacity
-              style={styles.flipButton}
-              onPress={this.toggleFacing.bind(this)}>
-              <Text style={styles.flipText}> FLIP </Text>
-            </TouchableOpacity>
-          </View>
-        </View>
+          }}></View>
 
         <View style={{bottom: 0}}>
           <View
@@ -596,19 +292,6 @@ export default class VoiceDisable extends React.Component {
               ]}
               onPress={this.openYoutube.bind(this)}>
               <Text style={styles.flipText}> VIDEO </Text>
-            </TouchableOpacity>
-            <TouchableOpacity
-              style={[
-                this.state.buttonsHover.calenderButton
-                  ? styles.selectedButton
-                  : styles.flipButton,
-                this.state.buttonsHover.calenderButton
-                  ? styles.selectedPicButton
-                  : styles.picButton,
-                {flex: 0.24, alignSelf: 'flex-end'},
-              ]}
-              onPress={this.setEventToCalender.bind(this)}>
-              <Text style={styles.flipText}> CALENDAR </Text>
             </TouchableOpacity>
           </View>
         </View>
